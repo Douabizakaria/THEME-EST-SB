ba209d35f4965c82b1417c242f0847f59a2f943b
DOUABIZAKARIA <58117479+Douabizakaria@users.noreply.github.com>
2021-03-27 14:51:18 +0100

    Add files via upload

---
+++ THEME EST-SB/functions.php
@@ -0,0 +1,96 @@
+<?php
+	/*
+	**
+	**
+	**
+	**
+	*/
+
+		require_once('wp-bootstrap-navwalker.php');
+		add_theme_support( 'post-thumbnails' );
+
+function load_stylesheets()
+    {
+        
+        wp_register_style('bootstrap',get_template_directory_uri() . '/css/bootstrap.min.css' , 
+        
+        array() , false , 'all') ;
+        
+        wp_enqueue_style('bootstrap');
+
+        wp_register_style('style',get_template_directory_uri() . '/main.css' , 
+        
+        array() , false , 'all') ;
+        
+        wp_enqueue_style('style');
+
+    }
+add_action('wp_enqueue_scripts' , 'load_stylesheets') ;
+
+
+
+function include_jquery()
+        
+        {
+
+            wp_deregister_script('jquery');
+            wp_enqueue_script('jquery' , get_template_directory_uri() . '/js/jquery.min.js','',1,true);
+
+     
+
+        }
+
+        add_action('wp_enqueue_scripts' , 'include_jquery');
+
+
+
+
+function loadjs()
+    
+    {
+
+        wp_register_script('customjs',get_template_directory_uri() . '/js/scripts.js','',1,true);
+        wp_enqueue_script('customjs');
+
+    }
+    add_action('wp_enqueue_scripts','loadjs');
+
+
+    
+
+
+
+function register_menu(){
+
+	register_nav_menus(array(
+
+		'boostrap-menu'=>'navigation menu',
+		'footer-menu'=>'footer bar'
+
+	));
+}
+add_action('init','register_menu');
+
+function boostrab_nav_menu(){
+
+	wp_nav_menu(
+		array(
+			
+			'theme_location'	=>'boostrap-menu',
+			'menu_class'		=>'navbar-nav ',
+			'container'			=>false,
+			'depth'				=>2,
+			'walker'			=> new wp_bootstrap_navwalker()
+			
+			
+
+
+
+		));
+
+}
+
+
+
+
+
---
+++ THEME EST-SB/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+
+	<title>Accueil |  PAGE ACCUEUIL DE L'EST &lt;em&gt; DE SIDI BENNOUR !&lt;/em&gt;</title>
+
+		
+</head>
+
+
+<body>
+	
+
+	<div class="footer">
+		<div class="container">
+			<div class="row justify-content-start">
+				<div class="col">
+					<div class="footer-content">
+						<span class="color_11">CONTACT:&nbsp;<br> 00 212 5 23 37 93 01  &nbsp; <br> Contacter Nous sur : <a href="mailto:info@monsite.fr" target="_self">zakaria.douabi11@outlook.fr</a>   </span><br><hr><span class="color_13">ADRESSE  <p>      </p> </span><span style="font-weight:bold;">Route nationale N°1 El haouziya</span>  <hr>HORAIRES D'OUVERTURE <br> <span class="color_11">Lun-Ven&nbsp;: 10h - 17h    </span>   
+					</div>
+
+					<ul class="navbar-nav ml-auto"><li class="nav-item active"><a href="http://127.0.0.1:53264/index.html" rel="" class="nav-link">Accueil</a></li><li class="nav-item"><a href="http://127.0.0.1:53264/contact-form/index.php" rel="" class="nav-link">informer Nous !</a></li></ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+</body>
+</html>
